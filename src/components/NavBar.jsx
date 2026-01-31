ee2c2aaf2320ca50a1e161c219ff4536ea143bc3
Facundo <facugsuarez@hotmail.com>
2026-01-31 00:51:02 -0300

    final

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,10 @@ function NavBar() {
   return (
     <nav className="navbar">
       <Link to="/" className="nav-logo">
-        <img src="/alohomorareact-/medios/alohomora.png" alt="logo" />
+        <img
+          src={import.meta.env.BASE_URL + "medios/alohomora.png"}
+          alt="logo"
+        />
       </Link>
 
       <div className="nav-links">
